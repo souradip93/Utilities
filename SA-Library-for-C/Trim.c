1760bc1ffdf419f473a50b0f71aa53cd9707fc9d
Souradip Guha <souradip.guha@yahoo.com>
2016-07-08 11:55:13 +0530

    Update Trim.c

--- SA-Library-for-C/Trim.c
+++ SA-Library-for-C/Trim.c
@@ -4,7 +4,15 @@
 
 #define SA_TRIM(str, ...) sa_trim(str, (' ', ##__VA_ARGS__))
 #define SA_TRIM_CONSTANT(out, str, ...) sa_trim_constant(out, str, (' ', ##__VA_ARGS__))
-
+/*
+    Input       - Pointer of type char * that will point to the String, character d which is the character to be trimmed.
+    Output      - Return a pointer of type char * which points to the substring after trimming.
+    Function    - Trims the second input parameter from string pointed by str and returns a pointer that is pointing
+                    to a string with trimming done from begining and ending.
+    
+    NOTE :- 1) Never override the input pointer with the output of this function.
+            2) This function will work only for inputs where the string can be modified.
+*/
 char * sa_trim(char * str, char d){
     
     size_t len = strlen(str);
@@ -22,6 +30,17 @@ char * sa_trim(char * str, char d){
     return str;
 }
 
+/*
+    Input       - Pointer of type char * that will point to the output String, Pointer 
+                    of type char * that will point to the input String, character d which 
+                    is the character to be trimmed.
+    Output      - 
+    Function    - Trims the third input parameter from string pointed by str and sets the 
+                    pointer of first parameter to a string with trimming done from begining and ending.
+    
+    NOTE :- 1) This function will work for inputs where the string cannot be modified.
+            2) The ouput string must be long enough to hold the trimmed characters.
+*/
 void sa_trim_constant(char * out, char * str, char d){
     size_t len = strlen(str);
     
@@ -38,6 +57,9 @@ void sa_trim_constant(char * out, char * str, char d){
     out[end-start+1] = 0;
 }
 
+/*
+    TEST CASES
+*/
 void main() {
      
     char str[14] = " Souradip    ";
