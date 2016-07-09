bdd1afd0454fbc46eca10e6a48f7d62577d48008
Souradip Guha <souradip.guha@yahoo.com>
2016-07-10 01:05:23 +0530

    Create sa_trim.c

---
+++ SA-Library-for-C/implementation/sa_trim.c
@@ -0,0 +1,54 @@
+/*
+MIT License
+Copyright (c) 2016 Souradip Guha
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "../include/sa_trim.h"
+
+char * sa_trim(char * str, char d){
+    
+    size_t len = strlen(str);
+    
+    int start = 0;
+    int end = len - 1;
+    
+    while(str[start]==d && start<len) start++;
+    
+    while(str[end]==d && end>=0) end--;
+
+    str = str+start;
+    str[end-start+1] = 0;
+    
+    return str;
+}
+
+void sa_trim_constant(char * out, char * str, char d){
+    size_t len = strlen(str);
+    
+    int start = 0;
+    int end = len - 1;
+    
+    while(str[start]==d && start<len) start++;
+    
+    while(str[end]==d && end>=0) end--;
+
+    str = str+start;
+    
+    memcpy(out, str, end-start+1);
+    out[end-start+1] = 0;
+}
